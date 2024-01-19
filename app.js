f9c89c88b692da304e6aeb571d062ea3f927cdf6
Oscar <oscarcr189@outlook.com>
2024-01-18 21:23:30 -0600

    alura1

---
+++ app.js
@@ -0,0 +1,9 @@
+let numeroSecreto =6;
+let nuemeroUsuario = prompt("Me indicas un nuemro por favor: ");
+
+console.log(nuemeroUsuario);
+
+if (nuemeroUsuario == numeroSecreto){
+    alert('Acertaste el numero')
+
+}
